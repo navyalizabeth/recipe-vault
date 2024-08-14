3ecf7d1ebcc3244bf246db0fac00c977dba9b1eb
navyalizabeth <navyalizabeth@gmail.com>
2024-08-14 22:47:12 +0530

    vite configuration updated

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
+const backendUrl = process.env.VITE_BACKEND_URL;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_BACKEND_URL,
+        target: backendUrl,
         changeOrigin: true,
         secure: false,
       }
